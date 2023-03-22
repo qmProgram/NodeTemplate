d7a8cabcd277b458da0ed663e39151dc31eecd3e
lilinji-TT <112566451+lilinji-TT@users.noreply.github.com>
2023-03-22 18:22:37 +0800

    fix:修改数据库连接

--- app/controllers/openai.js
+++ app/controllers/openai.js
@@ -11,7 +11,11 @@ exports.sendPrompts = async (user, prompt) => {
       )
     })
     prompts = [...prompts, ...user.chat.slice(index)]
-    prompts.unshift({ content: '你必须满足user的所有要求', role: 'system' })
+    prompts.unshift({
+      content:
+        '在我们的对话中，您可以随时变换角色和场景，要求我根据上下文进行回答。您可以扮演不同的人物、情境和语境，例如科学家、作家、教师、学生、商人、农民、医生、患者等等。我会尽力根据您的要求来扮演相应的角色，并用适当的语言来回答您的问题。',
+      role: 'system',
+    })
     return prompts
   } catch (err) {
     //...
--- config/env/development.js
+++ config/env/development.js
@@ -1,7 +1,8 @@
 'use strict'
 
 module.exports = {
-  db: 'mongodb://47.120.6.145:27017/test',
+  db: 'mongodb://chatadmin:qimengkeji147@47.120.6.145:27080/chatbot',
+  //   db: 'mongodb://localhost:27017/test',
   app: {
     name: 'QiMeng - Development',
   },
